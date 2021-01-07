38196235b5a61f076d7152f7e91e7bffdbbb04b1
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-01-07 10:14:11 -0600

    Update count_non_duplicate.js

--- count_non_duplicate.js
+++ count_non_duplicate.js
@@ -9,8 +9,7 @@ function count_non_duplicate(s,window){
    let keys = Object.keys(dis);
    if(ins.length >= 0 || ins.length <= window)/*(keys.length >= 0 || keys.length <= window)*/{
      dis[s[deb]] = s[deb];
-      alert(dis);
-       if(keys.length == window){
+      if(keys.length == window){
          for( var i of keys){
           ins = ins + i;
          }
