b7b1378fc810425d1581febcae23e06828379ce8
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-01-11 12:10:42 -0600

    Update count_non_duplicate.js

--- count_non_duplicate.js
+++ count_non_duplicate.js
@@ -1,27 +1,48 @@
-// version 0.0.0.1
-
 function count_non_duplicate(s,window){
+
  var dis = {};
+
  //var ins = "";
+
  var res = {};
+
  var deb = 0;
+
  while(deb < s.length){
+
   dis[s[deb]] = s[deb];
+
   let getkeys = Object.keys(dis);
+
   if(getkeys.length == window){
-    let ins = "";
+
+    var ins = "";
+
     for(var i of getkeys){
+
       ins = ins + i;
+
       if(ins.length == window){
+
         res[ins] = ins.length;
+
       }
+
     }
-      ins = i;
-      dis = {}; 
-  } // end if 
+ 
+     ins = ""/*s[deb]*/;
+
+     dis = {};
+ 
+  } // end if
+ 
   deb++;
+
  } // end while
+
   return res;
+
 } //end function
 var test = "rtyuijjjklloopm";
-count_non_duplicate(test,3);
+
+count_non_duplicate(test,4)
