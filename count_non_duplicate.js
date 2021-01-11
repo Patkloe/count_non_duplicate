655469f7ffe4af740f62441e5bb631ce64ce8ca1
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-01-11 06:52:51 -0600

    Update count_non_duplicate.js

--- count_non_duplicate.js
+++ count_non_duplicate.js
@@ -1,26 +1,27 @@
 // version 0.0.0.1
 
 function count_non_duplicate(s,window){
-  var res = {};
-  var dis = {};
-  var ins = "";
-  var deb = 0;
-  while(deb < s.length){
-   let keys = Object.keys(dis);
-   if(ins.length >= 0 || ins.length <= window)/*(keys.length >= 0 || keys.length <= window)*/{
-     dis[s[deb]] = s[deb];
-      if(keys.length == window){
-         for( var i of keys){
-          ins = ins + i;
-         }
-          res[ins] = ins.length;
-          ins = s[deb]; // reinitialise ins
-          dis = {}; // on doit reinitialiser dis
-       }
-   }
-   deb++;
-  }
- return res;
-}
-var test = "aaadddrttyuiop";
-count_non_duplicate(test,4);
+ var dis = {};
+ //var ins = "";
+ var res = {};
+ var deb = 0;
+ while(deb < s.length){
+  dis[s[deb]] = s[deb];
+  let getkeys = Object.keys(dis);
+  if(getkeys.length == window){
+    let ins = "";
+    for(var i of getkeys){
+      ins = ins + i;
+      if(ins.length == window){
+        res[ins] = ins.length;
+      }
+    }
+      ins = i;
+      dis = {}; 
+  } // end if 
+  deb++;
+ } // end while
+  return res;
+} //end function
+var test = "rtyuijjjklloopm";
+count_non_duplicate(test,3);
